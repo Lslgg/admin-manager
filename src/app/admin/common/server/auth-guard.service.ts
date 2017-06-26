47df898e70280ef3ae19d7ef6112239defadf604
lslgg <449291161@qq.com>
2017-06-26 10:05:37 +0800

    ok

--- src/app/admin/common/server/auth-guard.service.ts
+++ src/app/admin/common/server/auth-guard.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Inject } from '@angular/core';
-import { AuthPower } from './shared/AuthPower';
 
 import {
     CanActivate, Router,
@@ -8,20 +7,51 @@ import {
     CanActivateChild
 } from '@angular/router';
 
-
+export class AuthPower {
+    public id: string;
+    public url: string;
+    public operation: Array<string> = new Array<string>();
+}
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanActivateChild {
 
     parseServer: ParserServer;
 
-    constructor(@Inject("parseManager") parse,
+    constructor(@Inject("parse") parse,
         private router: Router) {
         this.parseServer = parse;
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> | boolean {
-        return true;
+        let promise = new Promise<boolean>((resolve, reject) => {
+            var urlData = route.data;
+            let url = urlData["module"];
+            let power = urlData["power"];
+            if (url == "index" || url == "notPower" || url=="allpower") {
+                resolve(true);
+                return;
+            }
+
+            var isSuccess = false;
+            this.getRolrPowerList().then(list => {
+                list.forEach(val => {
+                    var index = val.operation.indexOf(power);
+                    if (val.url == url && index > -1) {
+                        isSuccess = true;
+                    }
+                });
+                resolve(true);
+                if (!isSuccess) {
+                    this.router.navigate(['/admin/notPower']);
+                    return;
+                }
+            }).catch(error => {
+                reject(false);
+            })
+        });
+
+        return promise;
     }
 
     canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> | boolean {
